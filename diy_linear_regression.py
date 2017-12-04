3ce648837c123b7501ca7a302711e7151f54d55b
notsambeck <thesamuelbeck@gmail.com>
2017-12-03 23:17:17 -0800

    work on diy_linear (still not done)

--- diy_linear_regression.py
+++ diy_linear_regression.py
@@ -1,7 +1,8 @@
 '''
 diy linear regression learning algorithm
 
-
+specifically: use height and gender as independent variables
+to estimate the weight of a person.
 '''
 
 import numpy as np
@@ -22,17 +23,14 @@ def rmse(y, y_pred):
 
 class DIYLinearRegression():
     '''
-    Logistic implements logistic regression using above functions;
-    a rough analog to sklearn.linear.LogisticRegression.
+    implements linear regression;
+    a rough analog to sklearn.linear.LinearRegression.
     additionally stores error and accuracy histories as lists.
 
     args:
         X, y: ndarrays with matching numbers of samples
-        metric: a function
-            args:
-                labels, predictions: ndarrays
-            returns:
-                loss: float
+
+        metric: a function that maps (y, preds) to loss
     '''
     def __init__(self, X, y, metric=rmse):
         print('logistic regression on with {} as metric'.format(metric))
@@ -62,30 +60,29 @@ class DIYLinearRegression():
         finds gradient of metric with respect to current weights:
         d(metric) / dw
 
-        'Dumb' because it is not computed (which would be relatively easy)
+        'Dumb' because it is not determined analytically
         but instead measured empirically across all samples.
 
         returns:
             gradient, error(metric), accuracy
         '''
-        error = metric(self.y, prob)
-        acc = accuracy(y, prob)
+        error = metric(self.y, self.predict(self.X))
         # print('error @ epoch start: {}; accuracy: {}'.format(error, acc))
         grad = []
         # print('input weights: {}: {}'.format(type(w), w))
-        for i in range(len(w)):
-            new_w = w.copy()
-            new_w[i] += step
+        for i in range(len(self.w)):
+            new_w = self.w.copy()
+            new_w[i] += self.step
             # print(new_w)
-            new_error = metric(y, predict_proba(X, new_w))
-            grad.append((error - new_error) / step)
+            new_error = metric(self.y, self.predict(self.X))
+            grad.append((error - new_error) / self.step)
 
         # grad is the change in error when w[i] is increased by .01
         # grad = dE/dw for w in weights
         # if grad[i] is positive,
-        # print('unnormalized grad: {:.5} {:.5} {:.5}'.format(*[g for g in grad]))
+        # print('unnormalized grad:{:.5}{:.5}{:.5}'.format(*[g for g in grad]))
 
-        return np.array(grad), error, acc
+        return np.array(grad), error
 
     def standardize(self):
         # standardize data
@@ -103,8 +100,11 @@ class DIYLinearRegression():
 
     def _train(self, v=1):
         '''train one epoch and print'''
-        grad, error, acc = dumb_gradient(self.w, self.X, self.y, self.step,
-                                         self.metric)
+        grad, error, acc = self.dumb_gradient(self.w,
+                                              self.X,
+                                              self.y,
+                                              self.step,
+                                              self.metric)
         if v:
             print('weights are: {:f} {:f} {:f}'.format(*self.w))
             print('gradient is: {:f} {:f} {:f}'.format(*grad))
@@ -124,17 +124,18 @@ class DIYLinearRegression():
         '''predict new array of [heights, weights]'''
         x = np.subtract(x, self.means)
         x = np.divide(x, self.stds)
-        return predict_proba(np.array(x).reshape(-1, 2), self.w)
+        return self.predict(x)
+
+    def predict(self, x):
+        '''predict already normalized data'''
+        return np.dot(np.concatenate([x, np.ones(x.shape[0], 1)]), self.w)
 
     def display(self, df):
         # make masks
-        pred_male = self.predict_new_sample(df[['Height', 'Weight']]) > .5
-        male = df.Gender == 1
+        df.Gender = (df.Gender == 'Male').astype(int)
+        pred_weight = self.predict_new_sample(df[['Height', 'Gender']])
 
-        y = df.Gender.values
-
-        print('diy logistic regression accuracy:')
-        print(accuracy(y, pred_male))
+        y = df.Weight.values
 
         fig, axes = plt.subplots(nrows=1, ncols=2)
         plt.title = 'gradient metric: {}'.format(self.metric)
@@ -148,9 +149,9 @@ class DIYLinearRegression():
                                alpha=.5, c='r', label='female')
 
         df[pred_male].plot.scatter(ax=axes[0], x='Height', y='Weight', s=.5,
-                                   c='k', label='pred_male')
+                                   c='k', label='male_preds')
         df[~pred_male].plot.scatter(ax=axes[0], x='Height', y='Weight', s=.5,
-                                    c='white', label='pred_female')
+                                    c='white', label='female_preds')
 
         # subplot 2: matplotlib
         plt.ylabel('score')
@@ -172,13 +173,13 @@ def main():
 
     # baseline
     print()
-    sklr = LogisticRegression()
+    sklr = LinearRegression()
     sklr.fit(X, y)
     print('Baseline SKlearn score:', sklr.score(X, y))
     print()
 
     # diy
-    lr = Logistic(X, y, metric=prob_error)
+    lr = DIYLinearRegression(X, y, metric=rmse)
     lr.train(150)
     lr.display(df)
 
